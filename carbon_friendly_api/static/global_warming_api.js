7918aa3b72d846e6cfc310a5bc26d2f4b4f2a5d9
Ramon Alvarez <ramon.paul.alvarez@gmail.com>
2021-07-20 16:00:52 -0400

    Fixed metrics loading UI.  Added Work on Climate to Career.

--- carbon_friendly_api/static/global_warming_api.js
+++ carbon_friendly_api/static/global_warming_api.js
@@ -1,40 +1,40 @@
 function setCurrentCo2(element_id){
-    $.ajax({
+    return $.ajax({
         type: "GET",
         dataType: "json",
         url: "https://global-warming.org/api/co2-api",
         success: function (data) {
             let co2_records = data["co2"];
             latest_record = co2_records[co2_records.length - 1];
-            $("#" + element_id).html(latest_record["trend"]);
+            $("#" + element_id).html(latest_record["trend"] + " CO2 PPM");
             $("#" + element_id).attr('title', `Carbon Dioxide in PPM (${latest_record["month"]}/${latest_record["day"]}/${latest_record["year"]})`);
         }
     });
 }
 
 function setCurrentTemperatureAnomaly(element_id){
-    $.ajax({
+    return $.ajax({
         type: "GET",
         dataType: "json",
         url: "https://global-warming.org/api/temperature-api",
         success: function (data) {
             let temperature_records = data["result"];
             latest_record = temperature_records[temperature_records.length - 1];
-            $("#" + element_id).html(latest_record["station"]);
+            $("#" + element_id).html(latest_record["station"] + " C");
             $("#" + element_id).attr('title', `Temperature Anomaly in Celsius (${latest_record["time"]})`);
         }
     });
 }
 
 function setCurrentMethane(element_id){
-    $.ajax({
+    return $.ajax({
         type: "GET",
         dataType: "json",
         url: "https://global-warming.org/api/methane-api",
         success: function (data) {
             let methane_records = data["methane"];
             latest_record = methane_records[methane_records.length - 1];
-            $("#" + element_id).html(latest_record["average"]);
+            $("#" + element_id).html(latest_record["average"] + " CH4 PPM");
             $("#" + element_id).attr('title', `Methane in PPM (${latest_record["date"]})`);
         }
     });
@@ -43,11 +43,10 @@ function setCurrentMethane(element_id){
 // Populate Metrics
 $(document).ready(function () {
     try {
-        setCurrentCo2("metric_co2");
-        setCurrentMethane("metric_ch4");
-        setCurrentTemperatureAnomaly("metric_t");
-        $('#metrics-loading').hide();
-        $('#metrics-ready').show();
+        $.when(setCurrentCo2("metric_co2"), setCurrentMethane("metric_ch4"), setCurrentTemperatureAnomaly("metric_t")).done(() => {
+            $('#metrics-loading').hide();
+            $('#metrics-ready').show();
+        });
     } catch (err) {
         console.log(err);
         $('#metrics').hide();
